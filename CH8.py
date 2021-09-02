03b89e1860d42f11bc6bb66d2e0d94ca2f33dec4
Sam <60146956+srudqvist@users.noreply.github.com>
2021-09-02 14:52:10 -0500

    Create CH8.py

---
+++ CH8.py
@@ -0,0 +1,44 @@
+
+
+print('Hello, Sam')
+
+# Deliverable 2
+age = 15
+
+if age >= 21:
+    print('We are going to Vegas!')
+    print('Feeling lucky!')
+else:
+    print('One of these days...')
+
+    
+# Deliverable 3
+pet = 'cat'
+
+if pet == 'dog':
+    print('Remember to buy dog biscuts')
+elif pet == 'cat':
+    print('Buy some catnip')
+else:
+    print('Buy something')
+
+# Deliverable 4
+day = 'Sunday'
+season = 'summer'
+
+if season == 'fall' and day == 'Sunday':
+    print('Football season is here!')
+else:
+    print('Football season is here')
+
+language = 'C#'
+
+if language == 'Python' or language == 'R':
+    print('Learn some machine language!')
+else:
+    print('Learn Python and R')
+
+for i in range(1,10):
+    print(i)
+
+
